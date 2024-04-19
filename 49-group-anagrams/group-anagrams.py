e23964df5deb9aab19cd9e858460618440e3f3a4
Amr Shoukry <121165225+AmrShoukry@users.noreply.github.com>
2024-04-19 07:31:14 +0200

    Time: 96 ms (33.42%) | Memory: 21.1 MB (32.17%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,18 +1,14 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         anagrams = {}
-        array = []
-        index = 0
         for string in strs:
             char_array = [0] * 26
             for char in string:
                 char_array[ord(char) - 97] += 1
-            try:
-                stored_index = anagrams[f"'{char_array}'"]
-                array[stored_index].append(string)
-            except:
-                anagrams[f"'{char_array}'"] = index
-                array.append([string])
-                index += 1
-        return array
+            key = anagrams.get(tuple(char_array), 'AmrShoukry')
+            if key == 'AmrShoukry':
+                anagrams[tuple(char_array)] = [string]
+            else:
+                anagrams[tuple(char_array)].append(string)
+        return list(anagrams.values())
         

665c278d9e15d8a41c0765e1189cf13b41c37f7a
Amr Shoukry <121165225+AmrShoukry@users.noreply.github.com>
2024-04-20 18:15:30 +0200

    Time: 89 ms (58.59%) | Memory: 21.2 MB (56.53%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,18 +1,21 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         iterations = {}
-        values = []
+        array_length = len(nums)
+        array = ['s'] * (array_length + 1)
+        found = 0
+        solution = []
         for num in nums:
             iterations[num] = iterations.get(num, 0) + 1
-        
-        for i in range(k):
-            maximum_value = -1
-            maximum_key = -1
-            for [key, value] in iterations.items():
-                if (value > maximum_value):
-                    maximum_value = value
-                    maximum_key = key
-            iterations[maximum_key] = -1
-            values.append(maximum_key)
-        
-        return values
+        for [key, value] in iterations.items():
+            if array[value] == 's':
+                array[value] = [key]
+            else:
+                array[value].append(key)
+        for i in range(array_length, -1 , -1):
+            for element in array[i]:
+                if (element != 's'):
+                    solution.append(element)
+                    found += 1
+                    if found == k:
+                        return solution

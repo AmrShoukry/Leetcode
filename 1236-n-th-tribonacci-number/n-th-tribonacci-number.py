cedfa3c61f59b0101dadf6edda0a710d17618817
Amr Shoukry <121165225+AmrShoukry@users.noreply.github.com>
2024-04-24 17:57:23 +0200

    Time: 35 ms (51.60%) | Memory: 16.5 MB (31.32%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
@@ -4,10 +4,10 @@ class Solution:
             return 0
         if n == 1 or n == 2:
             return 1
-        arr = [-1] * (n + 1)
-        arr[0] = 0
-        arr[1] = 1
-        arr[2] = 1
+        arr = [0, 1, 1]
         for i in range(3, n + 1):
-            arr[i] = arr[i - 1] + arr[i - 2] + arr[i - 3]
-        return arr[n]
+            final = arr[0] + arr[1] + arr[2]
+            arr[0] = arr[1]
+            arr[1] = arr[2]
+            arr[2] = final
+        return arr[2]

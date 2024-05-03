f5cf2af3f44e661a9f7ccc61699fb5ec23ce3233
Amr Shoukry <121165225+AmrShoukry@users.noreply.github.com>
2024-05-03 20:33:48 +0300

    Time: 444 ms (94.89%) | Memory: 29.9 MB (47.06%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.py
+++ 219-contains-duplicate-ii/contains-duplicate-ii.py
@@ -1,25 +1,13 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        pointer1 = 0
-        pointer2 = 0
+        pointer = 0
         length = len(nums)
         hashes = {}
 
-        while pointer2 < length:
-            if pointer2 - pointer1 <= k:
-                val = hashes.get(nums[pointer2], 0)
-                if val == 0:
-                    hashes[nums[pointer2]] = 1
-                else:
-                    return True
-                pointer2 += 1
-            else:
-                hashes[nums[pointer1]] = 0
-                val = hashes.get(nums[pointer2], 0)
-                if val == 0:
-                    hashes[nums[pointer2]] = 1
-                else:
-                    return True
-                pointer1 += 1
-                pointer2 += 1
+        while pointer < length:
+            if abs(hashes.get(nums[pointer], 99999) - pointer) <= k:
+                return True
+            hashes[nums[pointer]] = pointer
+            pointer += 1
+
         return False

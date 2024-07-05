fa8475fa61b26b99c229303c1b19000caf329f2c
Amr Shoukry <121165225+AmrShoukry@users.noreply.github.com>
2024-07-05 20:33:49 +0300

    Time: 67 ms (9.66%) | Memory: 16.7 MB (50.92%) - LeetSync

--- 692-top-k-frequent-words/top-k-frequent-words.py
+++ 692-top-k-frequent-words/top-k-frequent-words.py
@@ -2,23 +2,16 @@ class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
         length = len(words)
         iterations = {}
-        array = [-1] * (length + 1)
         solution = []
         for word in words:
             iterations[word] = iterations.get(word, 0) + 1
-        for key, value in iterations.items():
-            if array[value] == -1:
-                array[value] = [key]
-            else:
-                array[value].append(key)
-                array[value] = sorted(array[value])
-        for i in range(length, -1, -1):
-            if array[i] == -1:
-                continue
-            while array[i]:
-                solution.append(array[i].pop(0))
-                k -= 1
-                if k == 0:
-                    return solution
-
         
+        heap = []
+
+        for word, freq in iterations.items():
+            heapq.heappush(heap, (-freq, word))
+        
+        for i in range(k):
+            solution.append(heapq.heappop(heap)[1])
+
+        return solution

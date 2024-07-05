72c653badb48b9641c8f71f8c82c302f52a08898
Amr Shoukry <121165225+AmrShoukry@users.noreply.github.com>
2024-07-05 19:20:08 +0300

    Time: 44 ms (86.87%) | Memory: 21 MB (62.72%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
@@ -7,40 +7,14 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        common = [root]
-        found = [0, 0]
-        self.traverse(root, common, found, p.val, q.val)
-        return common[0]
-
-    def traverse(self, node, common, found, p, q):
-        if not node:
-            return
-        print("C", common[0].val)
-        print(node.val)
-        if node.val == p:
-            found[0] = 1
-            print("ppp")
-            if found[0] == 1 and found[1] == 1:
-                return
-            common[0] = node
-
-        if node.val == q:
-            found[1] = 1
-            print("qqq")
-            if found[0] == 1 and found[1] == 1:
-                return
-            common[0] = node
-
-
-        if found[0] == 0 and found[1] == 0:
-            common[0] = node
-        self.traverse(node.left, common, found, p, q)
-        if found[0] == 1 and found[1] == 1:
-            return
-        if node.left == common[0]:
-            common[0] = node
-        self.traverse(node.right, common, found, p, q)
-        if found[0] == 1 and found[1] == 1:
-            return
-        if node.right == common[0]:
-            common[0] = node
+        if not root or root == p or root == q:
+            return root
+        
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        
+        if left and right:
+            return root
+        if left:
+            return left
+        return right
